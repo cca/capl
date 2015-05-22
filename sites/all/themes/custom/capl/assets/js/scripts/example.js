3f79fdb2e29475c3c07c72828d85dac5b359c0ff
Aaron Spafford <aaron.spafford@gmail.com>
2015-05-21 17:06:49 -0700

    testing react

--- sites/all/themes/custom/capl/assets/js/scripts/example.js
+++ sites/all/themes/custom/capl/assets/js/scripts/example.js
@@ -1,40 +1,46 @@
-var UserGist = React.createClass({
+var Project = React.createClass({
+
+  render: function() {
+    <div className='project'>
+      <span>{this.props.title}</span>
+    </div>
+
+  }
+
+});
+
+var ProjectList = React.createClass({
+
   getInitialState: function() {
     return {
-      title: ''
+      dataVar1: ''
     };
   },
 
-  getInitialState: function(){
-    return {data: {title: []}};
-  },
-
   componentDidMount: function() {
-    $.get(this.props.source, function(result) {
-      var lastGist = result[0];
+    $.get(this.props.projects, function(result) {
       if (this.isMounted()) {
         this.setState({
-          //title: lastGist.title
-          data: result
+          dataVar1: result
         });
+
       }
     }.bind(this));
   },
 
   render: function() {
+    
+    console.log(this.state.dataVar1);
+
     return (
       <div>
-      this.props.data.participants.map(function(player) {
-   return <li key={player.title}>{player.title}</li>
-})
-
-        //{this.state.title} last gist is
+        {this.state.dataVar1}
       </div>
     );
   }
 });
 
 React.render(
-  <UserGist source="http://localhost:8888/nodes/projects" />,
+  <ProjectList projects="http://capl.local/nodes/projects" />,
   document.getElementById('content')
-);;
+);
