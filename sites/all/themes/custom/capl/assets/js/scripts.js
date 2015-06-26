a6a205b82d28469e2ecd90d6fe17e169a7e0be90
Aaron Spafford <aaron.spafford@gmail.com>
2015-06-26 13:45:54 -0700

    add sticky header script

--- sites/all/themes/custom/capl/assets/js/scripts.js
+++ sites/all/themes/custom/capl/assets/js/scripts.js
@@ -35,6 +35,57 @@
     }
   }
 
+  Drupal.behaviors.header = {
+    attach: function(context, settings) {
+      // Hide Header on on scroll down
+      var didScroll;
+      var lastScrollTop = 0;
+      var delta = 5;
+      var navbarHeight = $('.logo').outerHeight();
+
+      console.log(navbarHeight);
+
+      $(window).scroll(function(event){
+          didScroll = true;
+      });
+
+      setInterval(function() {
+          if (didScroll) {
+              hasScrolled();
+              didScroll = false;
+          }
+      }, 250);
+
+      function hasScrolled() {
+          var st = $(this).scrollTop();
+          
+          // Make sure they scroll more than delta
+          if(Math.abs(lastScrollTop - st) <= delta)
+              return;
+          
+          // If they scrolled down and are past the navbar, add class .nav-up.
+          // This is necessary so you never see what is "behind" the navbar.
+          if (st > lastScrollTop && st > navbarHeight){
+              // Scroll Down
+              $('.logo, label[for="nav-trigger"]').removeClass('nav-down').addClass('nav-up');
+              $('.logo').removeClass('over');
+          } else {
+              // Scroll Up
+              if(st + $(window).height() < $(document).height()) {
+                  $('.logo, label[for="nav-trigger"]').removeClass('nav-up').addClass('nav-down');
+              }
+              if(st > navbarHeight) {
+                $('.logo').addClass('over');
+              } else {
+                $('.logo').removeClass('over');
+              }
+          }
+          
+          lastScrollTop = st;
+      }
+    }
+  }
+
   Drupal.behaviors.accordions = {
     attach: function (context, settings) {
 
