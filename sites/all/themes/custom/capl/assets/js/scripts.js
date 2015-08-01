763dfe6a8bbe1cd3014f5328d999646db51353c8
Aaron Spafford <aaron.spafford@gmail.com>
2015-07-31 17:26:27 -0700

    Fix admin toolbar covering navbar

--- sites/all/themes/custom/capl/assets/js/scripts.js
+++ sites/all/themes/custom/capl/assets/js/scripts.js
@@ -11,9 +11,9 @@
       function adminMenuFix() {
         // Adjust body position when admin menu is present.
         if($('#admin-menu').length > 0) {
-          var bodyOffset = $('#admin-menu-wrapper').height() + 10;
+          var bodyOffset = $('#admin-menu-wrapper').height() + 5;
           $('body').removeClass('admin-menu').css('margin-top', bodyOffset, 'important');
-          $('label[for="nav-trigger"]').css('top', bodyOffset, 'important');
+          $('.navbar').css('top', bodyOffset);
         }
       }
 
